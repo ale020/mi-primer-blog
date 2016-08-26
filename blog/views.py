0ae6f6a398c1d6d5df0c0aeedb8c7fd008f60aff
ale <ale.gauto92@gmail.com>
2016-08-26 16:53:19 -0400

    modificamos templates para hacerlas dinamicas

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.utils import timezone
 # Create your views here.
+
+
 def lista_post(request):
-	return render(request, 'blog/lista_post.html',{})
+	post = Post.objects.filter(fecha_publicada__lte=timezone.now()).order_by('fecha_publicada')
+	return render(request, 'blog/lista_post.html',{'posts': post})
+
