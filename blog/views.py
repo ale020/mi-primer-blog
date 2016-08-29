cc2d1314a5d6bcacdae77ea1993db263c67f77b3
ale <ale.gauto92@gmail.com>
2016-08-29 12:12:23 -0400

    se agrego views, crear y editar posts en el lugar

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 # Create your views here.
@@ -8,3 +8,6 @@ def lista_post(request):
 	post = Post.objects.filter(fecha_publicada__lte=timezone.now()).order_by('fecha_publicada')
 	return render(request, 'blog/lista_post.html',{'posts': post})
 
+def detalle_post(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	return render(request, 'blog/detalle_post.html',{'post':post})
