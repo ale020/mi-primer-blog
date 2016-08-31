776c7fce4578d4d243b03a10c030d5f944636449
ale <ale.gauto92@gmail.com>
2016-08-31 14:04:09 -0400

    se agregaron nuevos views en el sitio

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.lista_post),
 	url(r'^post/(?P<pk>[0-9]+)/$',views.detalle_post),
+	url(r'^post/nuevo/$', views.nuevo_post, name='nuevo_post'),
+	url(r'^post/(?P<pk>[0-9]+)/edita/$', views.edita_post, name='edita_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
+from .forms import PostForm
 # Create your views here.
 
 
@@ -11,3 +12,31 @@ def lista_post(request):
 def detalle_post(request,pk):
 	post = get_object_or_404(Post,pk=pk)
 	return render(request, 'blog/detalle_post.html',{'post':post})
+
+def nuevo_post(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.fecha_publicada = timezone.now()
+			post.save()
+			return redirect('blog.views.detalle_post',pk=post.pk)
+	else:
+		form = PostForm()
+	
+
+	return render(request,'blog/edita_post.html', {'form': form})
+
+def edita_post(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == 'POST':
+		form = PostForm(request.POST, instance=post) 
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.autor = request.user
+			post.save()
+		return redirect('blog.views.detalle_post',pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request,'blog/edita_post.html',{'form':form})
